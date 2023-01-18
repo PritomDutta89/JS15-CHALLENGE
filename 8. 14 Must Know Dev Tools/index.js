842e0eeef5c6a258d6e80b30beebe4712b6544bf
Pritom Dutta <duttapritom777@gmail.com>
2023-01-18 23:28:44 +0530

    Add must known dev tools

---
+++ 8. 14 Must Know Dev Tools/index.js
@@ -0,0 +1,40 @@
+// console.log("hello");
+
+// console.warn("hello");
+
+// console.error("hello");
+
+// console.info("hello");
+
+// console.assert( 1===2, "hehe")
+
+// console.clear();
+
+// console.dir()
+
+
+// const dog = [
+//     {name: "bruh"},
+//     {name: "remmi"}
+// ]
+
+// dog.forEach((e)=>{
+//     console.log(`name : ${e.name}`)
+// })
+
+
+// const h = document.querySelector("h1");
+
+// console.dir(h)
+
+
+// console.count("object");
+// console.count("object");
+// console.count("object");
+// console.count("object");
+// console.count("object");
+
+
+// console.time()
+
+// console.table();
